420b6f3d532d786f12a5ac32673d848857447124
Yera08 <145241504+Yera08@users.noreply.github.com>
2024-07-02 18:56:48 +0500

    Add files via upload

---
+++ src/main/java/kz/yerzhan/mycomposeanim/MainActivity.kt
@@ -0,0 +1,55 @@
+package kz.yerzhan.mycomposeanim
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeAnim()
+                }
+            }
+        }
+
+@Composable
+fun ComposeAnim(){
+    Card {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+        Column(modifier = Modifier
+            .size(width = 200.dp, height = 200.dp)
+            .clickable { expanded = !expanded },
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Image(painter = painterResource(R.drawable.jetpack_compose_icon), contentDescription = "")
+            AnimatedVisibility(visible = expanded) {
+                Text(text = "Hello Android",
+                    style = MaterialTheme.typography.bodyLarge)
+
+            }
+        }
+    }
+}
